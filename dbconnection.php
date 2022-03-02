0c5f2ed367dbee8b4d6b396f5a1ef696e19aaa62
Ru521 <yuru880521@gmail.com>
2022-03-02 19:12:07 +0800

    rename index.php

--- dbconnection.php
+++ dbconnection.php
@@ -2,8 +2,7 @@
 //DB_Server, DB_Name, DB_Username, DB_Password	
 try {
     $conn = new PDO("sqlsrv:Server=localhost;Database=IM_choose_class", "sa", "1qaz@WSX");
-    if($conn)
-        echo 'success<br>';
+    
 } catch (PDOException $e) {
     print "資料庫連接失敗，訊息:{$e -> getMessage()}<br/>";
     die();
